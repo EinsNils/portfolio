7f861814e0c95e1d4f6929ab6162315a9b38e71b
Nils <nils.zentgraf@aol.com>
2024-05-21 19:13:25 +0200

    create MaxWidthWarpper component and css gradients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <MaxWidthWrapper>
+      <div className="gradient-01 absolute inset-0 z-50 h-20 w-1/2"></div>
+      <div className="gradient-03 z-0"></div>
+    </MaxWidthWrapper>
+  );
 }
